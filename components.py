4b08d72c9f5994e912a68c45379a83fb7982b70e
jiaping_wang <wangjp9802@gmail.com>
2017-10-24 20:51:08 -0400

    Update components.py

--- components.py
+++ components.py
@@ -1,21 +1,21 @@
 class Student(object):
     """A student is a class containing unique identity and a list of preference classes"""
-    id = -100
-    classes = []
     def __init__(self,id, classes):
         self.id = id
         self.classes = classes
 
 class ClassRoom(object):
-    capacity = -100
-    def __init__(self,size):
+    """A classroom is a room with specific size"""
+    def __init__(self,id,size):
+        self.id = id
         self.capacity = size
 
 
 class Course(object):
-    name = ""
-    teacher = ""
-    students = []
+    """A course is an object with unique name, 
+                                  unique teacher(for the basic version), and 
+                                  students who wants to take it(will be determined by random choice for bacic version)
+    """
     def __init__(self,name,teacher):
         self.name = name
         self.teacher = teacher
