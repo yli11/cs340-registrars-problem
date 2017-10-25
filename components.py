ecad95240338207552cb98ce243a7505f6882424
jiaping_wang <wangjp9802@gmail.com>
2017-10-24 20:34:41 -0400

    Update components.py

--- components.py
+++ components.py
@@ -1,15 +1,21 @@
 class Student(object):
     """A student is a class containing unique identity and a list of preference classes"""
+    id = -100
+    classes = []
     def __init__(self,id, classes):
         self.id = id
         self.classes = classes
 
 class ClassRoom(object):
+    capacity = -100
     def __init__(self,size):
         self.capacity = size
 
 
 class Course(object):
+    name = ""
+    teacher = ""
+    students = []
     def __init__(self,name,teacher):
         self.name = name
         self.teacher = teacher
