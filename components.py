67bf1e414a4c474a297c348ab5736f7d1729272e
yli11 <yli11@users.noreply.github.com>
2017-10-24 20:56:27 -0400

    Add default value to constructors

--- components.py
+++ components.py
@@ -1,12 +1,12 @@
 class Student(object):
     """A student is a class containing unique identity and a list of preference classes"""
-    def __init__(self,idx, classes):
+    def __init__(self,idx, classes=[]):
         self.idx = idx
         self.classes = classes
 
 class ClassRoom(object):
     """A classroom is a room with specific size"""
-    def __init__(self,idx,size):
+    def __init__(self,idx,size=0):
         self.idx = idx
         self.capacity = size
 
@@ -16,7 +16,7 @@ class Course(object):
                                   unique teacher(for the basic version), and 
                                   students who wants to take it(will be determined by random choice for bacic version)
     """
-    def __init__(self,name,teacher):
+    def __init__(self,name,teacher, specs=[]):
         self.name = name
         self.teacher = teacher
-        self.students = []
+        self.specs = specs
